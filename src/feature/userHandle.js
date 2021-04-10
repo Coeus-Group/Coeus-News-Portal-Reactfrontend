78b12097da7039298048d7166dd8e0fbf5128a2c
Alieu <alieubsagnia@gmail.com>
2021-04-10 21:57:37 +0100

    implemented user slice and reducer

--- src/feature/userHandle.js
+++ src/feature/userHandle.js
@@ -24,3 +24,17 @@ const userHandle = createSlice({
     },
 
 });
+
+export const {
+    setSignedIn,
+    SetUserData,
+    setInput,
+    setBlogData,
+} = userSlice.actions;
+
+export const selectUserInput = (state) => state.user.searchInput;
+export const selectBlogData = (state) => state.user.blogData;
+export const selectUserData = (state) => state.user.userData;
+export const selectSignedIn = (state) => state.user.isSignedIn;
+
+export default userSlice.reducer;
