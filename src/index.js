fc7efcba6a4b19822775702511463dabfdbaa525
Alieu <alieubsagnia@gmail.com>
2021-04-10 22:08:55 +0100

    fixed provider not found error

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import { Provider } from "react-redux";
 import store from "./app/store";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
