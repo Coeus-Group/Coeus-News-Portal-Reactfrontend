cca4976e1d9bb5a0919fbc87bcada4e7a7ae4c8c
Alieu <alieubsagnia@gmail.com>
2021-04-10 21:54:21 +0100

    State handling for userdata from login and search

--- src/feature/userHandle.js
+++ src/feature/userHandle.js
@@ -1,2 +1,26 @@
 import {createSlice} from "@reduxjs/toolkit";
 
+const userHandle = createSlice({
+    name : "user",
+    initialState: {
+        isSignedIn: false,
+        userData: null,
+        searchInput: "uk news",
+        blogData: null,
+    },
+    reducers: {
+        setSignedIn: (state, action) => {
+            state,isSignedIn = action.payload;
+        },
+        setUserData: (state, action) => {
+            state = userData = action.payload;
+        },
+        setInput: (state, action) => {
+            state.searchInput = action.payload;
+        },
+        setBlogData: (state, action) => {
+            state.blogData = action.payload;
+        },
+    },
+
+});
