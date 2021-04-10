3133de6a595934b9e686fafe1a435173388ae7ae
Alieu <alieubsagnia@gmail.com>
2021-04-10 22:47:26 +0100

    Landing page signin + css + google clientId

---
+++ src/components/landingpage.js
@@ -0,0 +1,56 @@
+import React from "react";
+import GoogleLogin from "react-google-login";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectSignedIn,
+  setSignedIn,
+  setUserData,
+} from "../features/userSlice";
+
+import "../styling/home.css";
+
+const Homepage = () => {
+  const isSignedIn = useSelector(selectSignedIn);
+
+  const dispatch = useDispatch();
+  const login = (response) => {
+    console.log(response);
+    dispatch(setSignedIn(true));
+    dispatch(setUserData(response.profileObj));
+  };
+
+  return (
+    <div className="home__page" style={{ display: isSignedIn ? "none" : "" }}>
+      {!isSignedIn ? (
+        <div className="login__message">
+          <h2>📗</h2>
+          <h1>Coues News Portal</h1>
+          <p>
+   ]        Online resource for reading blogs. Sign
+            up and start reading!.
+          </p>
+          <GoogleLogin
+            clientId="372908462482-gicrn8r4bg3t2j0fs9vt5av62agmvlq8.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="login__button"
+              >
+                Login with Google
+              </button>
+            )}
+            onSuccess={login}
+            onFailure={login}
+            isSignedIn={true}
+            cookiePolicy={"single_host_origin"}
+          />
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default Homepage;
