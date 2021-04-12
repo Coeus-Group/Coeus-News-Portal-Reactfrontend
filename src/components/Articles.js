0ff5031c74695cea4705904a01da971a6f73dd63
Alieu <alieubsagnia@gmail.com>
2021-04-12 19:02:13 +0100

    Display message when no blogs found

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,49 +1,57 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
 import { selectUserInput, setBlogData } from '../feature/userHandle';
-import {useDispatch, useSelector} from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 
 const Articles = () => {
     const searchInput = useSelector(selectUserInput);
-    const articles_url =`https://gnews.io/api/v4/search?q=${searchInput}&token=cc9053584965ba9e9062087c7c0f4011`
+    const articles_url = `https://gnews.io/api/v4/search?q=${searchInput}&token=cc9053584965ba9e9062087c7c0f4011`
     const dispatch = useDispatch();
-    const [blogs,setBlogs] = useState();
+    const [blogs, setBlogs] = useState();
 
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         axios
-         .get(articles_url)
-         .then ((response) => {
-            dispatch(setBlogData(response.data));
-            setBlogs(response.data);
-            setLoading(false);
-        })
-        .catch((error) => {
-            console.log(error);
+            .get(articles_url)
+            .then((response) => {
+                dispatch(setBlogData(response.data));
+                setBlogs(response.data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.log(error);
 
-        });
+            });
     }, [searchInput]);
 
     return (
         <div className="blog-page">
             <h1 className="blog-page-header">Articles</h1>
-             {loading ? <h1 classname="loading">Loading...♻️</h1>}
-        <div className="blogs">
-        {blogs?.articles?.map((blog) => (
-          <a className="blog" target="_blank" href={blog.url}>
-            <img src={blog.image} />
-            <div>
-              <h3 className="sourceName">
-                <span>{blog.source.name}</span>
-                <p>{blog.publishedAt}</p>
-              </h3>
-              <h1>{blog.title}</h1>
-              <p>{blog.description}</p>
+            {loading ? <h1 classname="loading">Loading...♻️</h1>}
+            <div className="blogs">
+                {blogs?.articles?.map((blog) => (
+                    <a className="blog" target="_blank" href={blog.url}>
+                        <img src={blog.image} />
+                        <div>
+                            <h3 className="sourceName">
+                                <span>{blog.source.name}</span>
+                                <p>{blog.publishedAt}</p>
+                            </h3>
+                            <h1>{blog.title}</h1>
+                            <p>{blog.description}</p>
+                        </div>
+                    </a>
+                ))}
+
+                {blogs?.totalArticles == 0 && (
+                    <h1 className="no__blogs">
+                        No blogs found 😵. Please try searching another key-word 
+                    </h1>
+                )}
             </div>
-          </a>
-        ))}
+        </div>
     );
 };
 
