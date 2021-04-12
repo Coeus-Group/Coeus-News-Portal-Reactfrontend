8db40331115abc2e8b172b694987213fcc023aa3
Alieu <alieubsagnia@gmail.com>
2021-04-12 16:30:49 +0100

    Gnews api token

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -4,7 +4,8 @@ import { selectUserInput } from '../feature/userHandle';
 
 const Articles = () => {
     const searchInput = useSelector(selectUserInput);
-    const articles_url =`https://gnews.io/api/v4/search?q=${searchInput}&token=API-Token`
+    const articles_url =`https://gnews.io/api/v4/search?q=${searchInput}&token=cc9053584965ba9e9062087c7c0f4011`
+    
     return (
         <div className="blog-page">
             <h1 className="blog-page-header">Articles</h1>
