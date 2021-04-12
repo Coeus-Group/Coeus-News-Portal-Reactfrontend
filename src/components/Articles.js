6b17803b5cc9d9a301874b24a3e1528b500234e2
Alieu <alieubsagnia@gmail.com>
2021-04-12 14:57:07 +0100

    Rename to Articles

---
+++ src/components/Articles.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Articles = () => {
+    return (
+        <div className="blog-page">
+            <h1 className="blog-page-header">Articles</h1>
+            
+        </div>
+    );
+};
+
+export default Articles;
