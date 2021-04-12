11ee8bc543d86043d3771dccd04876b712355ae3
Alieu <alieubsagnia@gmail.com>
2021-04-12 10:50:45 +0100

    Nav bar avatar + search + google login feature

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,47 +9,71 @@ import {
     setInput,
     setSignedIn,
     setUserData,
-}from "../feature/userHandle";
+} from "../feature/userHandle";
 
 const Navbar = () => {
 
-  const [inputValue, setInputValue] = useState("london");
-  const isSignedIn = useSelector(selectSignedIn);
-  const userData = useSelector(selectUserData);
+    const [inputValue, setInputValue] = useState("london");
+    const isSignedIn = useSelector(selectSignedIn);
+    const userData = useSelector(selectUserData);
 
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const logout = (response) => {
-    dispatch(setSignedIn(false));
-    dispatch(setUserData(null));
-  };
+    const logout = (response) => {
+        dispatch(setSignedIn(false));
+        dispatch(setUserData(null));
+    };
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    dispatch(setInput(inputValue));
-  };
+    const handleClick = (e) => {
+        e.preventDefault();
+        dispatch(setInput(inputValue));
+    };
 
 
-  return (
-    <div className="navbar">
-      <h1 className="navbar__header">Coeus News Portal 📰</h1>
-      {isSignedIn && (
-        <div className="blog__search">
-          <input
-            className="search"
-            placeholder="Search for a blog"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-          <button className="submit" onClick={handleClick}>
-            Search
+    return (
+        <div className="navbar">
+            <h1 className="navbar__header">Coeus News Portal 📰</h1>
+            {isSignedIn && (
+                <div className="blog__search">
+                    <input
+                        className="search"
+                        placeholder="Search for a blog"
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                    />
+                    <button className="submit" onClick={handleClick}>
+                        Search
           </button>
-        </div>
-      )}
+                </div>
+            )}
 
-      
-    </div>
-  );
+            {isSignedIn ? (
+                <div className="navbar__user__data">
+                    <Avatar
+                        className="user"
+                        src={userData?.imageUrl}
+                        alt={userData?.name}
+                    />
+                    <h1 className="signedIn">{userData?.givenName}</h1>
+                    <GoogleLogout
+                        clientId="372908462482-gicrn8r4bg3t2j0fs9vt5av62agmvlq8.apps.googleusercontent.com"
+                        render={(renderProps) => (
+                            <button
+                                onClick={renderProps.onClick}
+                                disabled={renderProps.disabled}
+                                className="logout__button"
+                            >
+                                Logout 🔴
+                            </button>
+                        )}
+                        onLogoutSuccess={logout}
+                    />
+                </div>
+            ) : (
+                <h1 className="notSignedIn">User not available 😞</h1>
+            )}
+        </div>
+    );
 };
 
 export default Navbar;
