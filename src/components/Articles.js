4ee002a22fd6c04fe661af31181444c191f86cd1
Alieu <alieubsagnia@gmail.com>
2021-04-12 15:10:15 +0100

    npm i axios + imported to articles+ searchbarinput

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import axios from 'axios'
+import { selectUserInput } from '../feature/userHandle';
 
 const Articles = () => {
+    const searchInput = useSelector(selectUserInput);
+
     return (
         <div className="blog-page">
             <h1 className="blog-page-header">Articles</h1>
