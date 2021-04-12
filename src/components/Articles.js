913e695c20ba01eb24800f76d68e303df91262a2
Alieu <alieubsagnia@gmail.com>
2021-04-12 16:38:50 +0100

    redux tool dispatch

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -5,7 +5,11 @@ import { selectUserInput } from '../feature/userHandle';
 const Articles = () => {
     const searchInput = useSelector(selectUserInput);
     const articles_url =`https://gnews.io/api/v4/search?q=${searchInput}&token=cc9053584965ba9e9062087c7c0f4011`
+    const dispatch = useDispatch();
+    const [blogs,setBlogs] = useState();
+
     
+
     return (
         <div className="blog-page">
             <h1 className="blog-page-header">Articles</h1>
