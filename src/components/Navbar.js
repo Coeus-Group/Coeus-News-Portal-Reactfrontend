a6072a91e8386d56666469e3b16d291f85fbadb6
Alieu <alieubsagnia@gmail.com>
2021-04-12 10:20:04 +0100

    Imported need libraries + Avatar from material-ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,15 @@
 import React from 'react'
+import { Avatar } from "@material-ui/core";
+import React, { useState } from "react";
+import { GoogleLogout } from "react-google-login";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    selectSignedIn,
+    selectUserData,
+    setInput,
+    setSignedIn,
+    setUserData,
+}from "../feature/userHandle";
 
 const Navbar = () => {
     return (
