fd2b76553f833865ccd360336e3a36a9a68869f0
sharada <sharada.telkar1988@gmail.com>
2021-04-14 16:26:28 +0530

    Validations added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -103,7 +103,7 @@ export default function SignIn() {
                         </Button>
                     <Grid container>
                         <Grid item>
-                            <Link href="#" variant="body2">
+                            <Link href="Dashboard.js" variant="body2">
                                 {"Don't have an account? Sign Up"}
                             </Link>
                         </Grid>
