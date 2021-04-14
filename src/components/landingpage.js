12210143cdab53b3e7b6097d82d47cdf9fdc7818
Alieu <alieubsagnia@gmail.com>
2021-04-15 00:49:09 +0100

    landing page rename

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -34,7 +34,7 @@ const Landingpage = () => {
         <div className="login__message ">
           <h2>📰</h2>
           <h1 className="typewriter-text" >Coues News Portal</h1>
-          {/* <p>Online resource for reading blogs. Signup and start reading!</p> */}
+          
 
 
           <Typewriter
