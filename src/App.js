ffe3701f16f18de51bd1ece79c754fc3007c4f92
Alieu <alieubsagnia@gmail.com>
2021-04-16 11:51:46 +0100

    News sub catergory pages created.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import { selectSignedIn } from "./feature/userHandle";
 import Navbar1 from "./components/Navbar1";
+import Breakingnews from "./components/Breakingnews";
+import Entertainmentnews from "./components/Entertainmentnews";
+import Worldnews from "./components/Worldnews";
 import "./styling/app.css";
 import Articles from './components/Articles';
 import { Navbar, Nav } from "react-bootstrap";
@@ -17,9 +20,10 @@ const App = () => {
   const isSignedIn = useSelector(selectSignedIn);
   return (
       <div className="app">
-        <Navbar /> 
+        <Navbar1 /> 
         <Landingpage />
         {isSignedIn && <Articles/>}
+        {isSignedIn && <Router/>}
 
         <Router>
       <header>
@@ -27,21 +31,20 @@ const App = () => {
           <Router>
             <main>
               <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Coeus News Portal</Navbar.Brand>
+               
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                   <Nav className="mr-auto">
-                    <Nav.Link ><Link to={"/Home"}>Home</Link></Nav.Link>
-                    <Nav.Link><Link to={"/Dashboard"}>Dashboard</Link></Nav.Link>
-                    <Nav.Link><Link to="/NewArticles">Create New Articles</Link></Nav.Link>
+                    <Nav.Link ><Link to={"/Breakingnews"}>Breaking News</Link></Nav.Link>
+                    <Nav.Link><Link to={"/Worldnews"}>World news</Link></Nav.Link>
+                    <Nav.Link><Link to={"/Entertainmentnews"}>Entertainment</Link></Nav.Link>
                   </Nav>
                 </Navbar.Collapse>
               </Navbar>
               <Switch>
-                <Route path="/Home" component={Home} />
-                <Route path="/Dashboard" component={Dashboard} />
-                <Route path="/NewArticles" component={NewArticles} />
-                <Route path="/Login" component={Login} />
+                <Route path="/Breakingnews" component={Breakingnews} />
+                <Route path="/Worldnews" component={Worldnews} />
+                <Route path="/Entertainmentnews" component={Entertainmentnews} />
               </Switch>
             </main>
           </Router>
