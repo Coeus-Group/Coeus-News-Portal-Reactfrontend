3f6dd7cf3c3ddab7a5626b9a1b917d0d149178e7
AlieuBSagnia <alieubsagnia@gmail.com>
2021-04-28 11:53:30 +0100

    Jest documentation now has an "official" workaround
    Test suite failed to run
    TypeError: window.matchMedia is not a function
    
    Co-Authored-By: sharada Telkar  <79594580+sharada-sudo@users.noreply.github.com>
    Co-Authored-By: srividyamulgund <40202144+srividyamulgund@users.noreply.github.com>

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// describe('Nav', () => {
+//   test('renders App component', () => {
+//     render(<Navbar1 />);
+ 
+//     expect(screen.getByText(/Search/)).toBeInTheDocument();
+//   });
+// });
+
